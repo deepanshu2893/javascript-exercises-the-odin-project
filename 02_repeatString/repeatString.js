a6ff53bfb6e53cd911d8d8b4f970bc52fe4a4e37
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-19 20:26:14 +0530

    js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+let string = "";
+let num = 10;
+let total = "";
+
+const repeatString = function (string, num) {
+    for (let i = 0; i < num; i++) {
+        total = total + string;
+    }
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        return total;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
