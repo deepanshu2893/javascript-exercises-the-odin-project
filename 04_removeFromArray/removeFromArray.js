739fc64055b2837acff2fff16ada51a386a37d62
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-20 14:29:12 +0530

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+let array = [1, 2, 3, 4];
+let number = 3;
 
+const removeFromArray = function (array, number) {
+    array.splice(number);
+    return array;
 };
 
 // Do not edit below this line
