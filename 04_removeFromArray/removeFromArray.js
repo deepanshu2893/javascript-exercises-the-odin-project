5e065b00240077699d5e12953830cc4c387d37c3
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-20 18:12:22 +0530

    4th exercise JS TOP

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,13 @@
-let array = [1, 2, 3, 4, 5];
-let number = 3;
+let array = [1, 2, 3, 4];
+let number1 = 3; 
+let number2 = 2;
 
-const removeFromArray = function (array, number) {
-    array.splice(number);
+const removeFromArray = function (array, number1, number2){
+    const indexToRemove = array.indexOf(number1);
+    const indexToRemove2 = array.indexOf(number2);
+    array.splice(indexToRemove, 1);
+    array.splice(indexToRemove2, 1);
     return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
