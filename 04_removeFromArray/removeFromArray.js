29b19113e25e762672f2b653cb5f518227e8722b
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-21 16:24:22 +0530

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
 let array = [1, 2, 3, 4];
-let number1 = 3; 
+let number1 = 3;
 let number2 = 2;
 
-const removeFromArray = function (array, number1, number2){
+const removeFromArray = function (array, number1, number2) {
     const indexToRemove = array.indexOf(number1);
     const indexToRemove2 = array.indexOf(number2);
     array.splice(indexToRemove, 1);
